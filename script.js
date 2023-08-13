2fcd81538aae43b9eae7b13ed0bc828d1f0984b2
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let div = document.getElementById("line");
+
+let keyFrames = [
+	{transform : "rotate(0deg)"},
+	{transform : "rotate(360deg)"}
+]
+let animationProperty = {
+	duration: 2000,
+	iterations : Infinity
+}
+
+div.animate(keyFrames, animationProperty);
